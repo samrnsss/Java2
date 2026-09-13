5d180a5a618aa9a68a0eb6aeeb94b43190b806cd
Samreen Bano <samreen22g@gmail.com>
2026-09-13 15:09:50 +0530

    prime or not

--- continuee.java
+++ continuee.java
@@ -11,23 +11,23 @@
 // }
 
 //check if a number is prime or not 
-import java.util.Scanner;
-public class continuee {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        boolean isPrime = true;
+// import java.util.Scanner;
+// public class continuee {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         boolean isPrime = true;
 
-            for (int i = 2; i <= n-1; i++) {
-                if (n % i == 0) {
-                    isPrime = false; // Found a divisor, not prime
-                }
-            }
+//             for (int i = 2; i <= n-1; i++) {
+//                 if (n % i == 0) {
+//                     isPrime = false; // Found a divisor, not prime
+//                 }
+//             }
 
-        if (isPrime  == true) {
-            System.out.println(n + " is a prime number.");
-        } else {
-            System.out.println(n + " is not a prime number.");
-        }
-    }
-}
+//         if (isPrime  == true) {
+//             System.out.println(n + " is a prime number.");
+//         } else {
+//             System.out.println(n + " is not a prime number.");
+//         }
+//     }
+// }
