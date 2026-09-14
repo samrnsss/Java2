28ac6924e5f361e85f6fb1cf6177d6cd005c82f9
Samreen Bano <samreen22g@gmail.com>
2026-09-14 20:37:16 +0530

    function overloading

---
+++ functOverloading.java
@@ -0,0 +1,41 @@
+//function overloading using arguments
+// public class functOverloading {
+
+//     //func to cal sum of two nums 
+//     public  static int sum(int a, int b){
+//         return a + b;
+//     }
+//     //func to cal sum of three nums
+//     public static int sum(int a, int b, int c){
+//         return a + b + c;
+//     }
+
+//     public static void main (String[] args) {
+//         System.out.println(sum(2, 3));
+//         System.out.println(sum(2, 3, 4));
+//     }
+// }
+
+//function overloading using data types
+// public class functOverloading {
+
+//     //func to cal sum of two nums 
+//     public  static int sum(int a, int b){
+//         return a + b;
+//     }
+//     //func to cal sum of two nums
+//     public static double sum(double a, double b){
+//         return a + b;
+//     }
+//     //func to cal float sum 
+//     public static float sum(float a, float b){
+//         return a + b;
+//     }
+
+//     //main func
+//     public static void main (String[] args) {
+//         System.out.println(sum(2, 3));
+//         System.out.println(sum(2.5, 3.5));
+//         System.out.println(sum(2.5f, 3.5f));
+//     }
+// }
