deb36e2a588e7a0820b9b73fa555668390dfe258
Samreen Bano <samreen22g@gmail.com>
2026-09-14 19:57:12 +0530

    call by value

---
+++ callByValue.java
@@ -0,0 +1,21 @@
+
+// public class callByValue{
+//     public static int calculateSum(int num1, int num2){
+//     int sum = num1 + num2;
+//     return sum;
+//     }
+
+//     public static void main(String[] args) {
+//         //swap - values exchange
+//         int a = 10;
+//         int b = 20;
+
+
+//         //swap 
+//         int temp = a;
+//         a = b;
+//         b = temp;
+
+//         System.out.println("After swapping: a = " + a + ", b = " + b);
+//     }
+// }
