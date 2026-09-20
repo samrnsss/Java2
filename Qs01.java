69d11a5187ade910646ea972e5c0e55cd2aefac6
Samreen Bano <samreen22g@gmail.com>
2026-09-20 13:03:04 +0530

    Trapped rainwater sum

--- Qs01.java
+++ Qs01.java
@@ -5,5 +5,35 @@
 //height=[4,2,0,6,3,2,5]
 
 public class Qs01 {
-    
+    public static int trappedWater(int[] height){
+        //calculate left max array
+        int n = height.length;
+        int leftMax[] = new int[n];
+        leftMax[0]= height[0];
+        for(int i =1; i<n; i++){
+            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        }
+        //calculate right max boundary 
+        int rightMax[] = new int[n];
+        rightMax[n-1] = height[n-1];
+        for(int i = n-2; i>=0; i--){
+            rightMax[i] = Math.max(height[i], rightMax[i+1]); 
+        }
+        int trappedWater =0;
+        
+        //loop
+        for(int i=0; i<n; i++){
+
+            //water level = min(leftMax, rightMax)
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+            //trapped water = water level - height[i]
+            trappedWater += waterLevel - height[i];
+        }
+        return trappedWater;
+    }
+    public static void main(String[] args) {
+        int[] height = {4,2,0,6,3,2,5};
+        trappedWater(height);
+        System.out.println("Trapped water is: " + trappedWater(height));
+    }
 }
